62371cbf0326301292410993f4d830a49b217bdd
anuraghithub-rgb <anuraghithub@gmail.com>
2026-05-03 09:16:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@
 import os
 
 # API Credentials (TERI INFO DAL)
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH", "")
-PHONE_NUMBER = os.environ.get("PHONE_NUMBER", "")
+API_ID = int(os.environ.get("API_ID", 39447635))
+API_HASH = os.environ.get("API_HASH", "fc12fa4f90b177af21e2648441bcde59")
+PHONE_NUMBER = os.environ.get("PHONE_NUMBER", "4915773609881")
 
 # Volume Settings (500% BOOST)
 MAX_VOLUME = 500  # 0-500 (500 = 5x boost via FFmpeg)
